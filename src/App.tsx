4461d4ac87ef67f7d4e0c30c804e684647abcc3f
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-03-12 13:50:39 +0530

    Material React Table Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
+import { useEffect, useState} from 'react';
+import {
+    MaterialReactTable,
+    useMaterialReactTable,
+    type MRT_ColumnDef,
+  } from 'material-react-table';
+
+  interface TableConfig {
+    enableRowSelection: boolean;
+    enableFilters: boolean;
+    enableColumnFilterModes: boolean;
+    enableColumnActions: boolean;
+  }
+
+  interface RowData {
+    name: string;
+    age: number;
+  }
+
+  const defaultTableConfig: TableConfig = {
+    enableRowSelection: false,
+    enableFilters: false,
+    enableColumnFilterModes: false,
+    enableColumnActions: false,
+  };
 
 const App = () => {
+  const [tableProps,setTableProps] = useState<TableConfig>(defaultTableConfig);
+  const [columns,setColumns] = useState<MRT_ColumnDef<RowData>[]>([]);
+  const [data, setData] = useState<RowData[]>([]);
+  const table = useMaterialReactTable({
+        columns,
+        data,
+        ...tableProps
+      });
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('./Employees.json');
+        const jsonData = await response.json();
+        setData(jsonData.data);
+        setColumns(jsonData.columns);
+        setTableProps(jsonData.props);
+      }catch(error){
+        console.error(error);
+      }
+    }
+    fetchData();
+  },[])
   return (
-    <div>App</div>
+    <div>
+      <MaterialReactTable table={table} />;
+    </div>
   )
 }
 
