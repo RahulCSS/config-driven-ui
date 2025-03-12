ef24de1eeade22f5b11c5ce6ee71009b2bceaa12
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-03-12 19:43:41 +0530

    Table Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,66 @@ import {
     MaterialReactTable,
     useMaterialReactTable,
     type MRT_ColumnDef,
+    type MRT_TableState,
   } from 'material-react-table';
 
   interface TableConfig {
+    enableTopToolbar: boolean;
     enableRowSelection: boolean;
-    enableFilters: boolean;
-    enableColumnFilterModes: boolean;
     enableColumnActions: boolean;
   }
 
-  interface RowData {
+  type Person = {
     name: string;
+    userName: string;
     age: number;
-  }
+    email :string;
+    role: string;
+    status: string;
+    teams: string[];
+  };
 
   const defaultTableConfig: TableConfig = {
-    enableRowSelection: false,
-    enableFilters: false,
-    enableColumnFilterModes: false,
-    enableColumnActions: false,
+    enableTopToolbar: true,
+    enableRowSelection: true,
+    enableColumnActions: true,
   };
 
 const App = () => {
   const [tableProps,setTableProps] = useState<TableConfig>(defaultTableConfig);
-  const [columns,setColumns] = useState<MRT_ColumnDef<RowData>[]>([]);
-  const [data, setData] = useState<RowData[]>([]);
+  const [initialStates, setInitialStates] = useState<Partial<MRT_TableState<Person>>>({});
+  console.log(initialStates)
+  console.log(initialStates.pagination);
+  const [columns,setColumns] = useState<MRT_ColumnDef<Person>[]>([]);
+  const [data, setData] = useState<Person[]>([]);
+  
+  const fetchData = async () => {
+    try {
+      const response = await fetch('./Employees.json');
+      const jsonData = await response.json();
+      setData(jsonData.data);
+      setColumns(jsonData.columns);
+      setTableProps(jsonData.props);
+      setInitialStates(jsonData.initialStates);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
   const table = useMaterialReactTable({
         columns,
         data,
+        initialState: initialStates,
+        renderColumnActionsMenuItems: ({ internalColumnMenuItems }) => {
+          return internalColumnMenuItems.slice(0, 2);
+        },
         ...tableProps
-      });
-  
+  });
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('./Employees.json');
-        const jsonData = await response.json();
-        setData(jsonData.data);
-        setColumns(jsonData.columns);
-        setTableProps(jsonData.props);
-      }catch(error){
-        console.error(error);
-      }
-    }
     fetchData();
   },[])
+
   return (
     <div>
       <MaterialReactTable table={table} />;
